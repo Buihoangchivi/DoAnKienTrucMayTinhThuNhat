fcbd28bd59deb1ff3401eb4c4077189844348180
Le Minh Duc <55783362+zaqy203@users.noreply.github.com>
2020-05-12 22:49:59 +0700

    chinh sua khung Qint

--- DoAn_GUI/Resource.h
+++ DoAn_GUI/Resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by DoAnGUI.rc
 //
+#define ID_ChuyenDoi1                   3
 #define ID_TinhToan                     4
 #define IDD_DOAN_GUI_DIALOG             102
 #define IDR_MAINFRAME                   128
@@ -18,34 +19,26 @@
 #define IDC_STATIC_BIN1                 1007
 #define IDC_STATIC_BIN2                 1008
 #define IDC_STATIC_DEC2                 1009
-
-#define ID_ChuyenDoi1                   3
 #define ID_ChuyenDoi2                   1010
-
 #define IDC_SoThu1                      1011
-#define IDC_SoThu2                      1015
-
 #define IDC_Dec                         1012
 #define IDC_Bin                         1013
 #define IDC_Hex                         1014
+#define IDC_SoThu2                      1015
 #define IDC_KQDec1                      1036
-#define IDC_KQDec2                      1043
-
 #define IDC_KQBin1                      1038
-#define IDC_KQBin2                      1044
-
 #define IDC_KQHex                       1039
 #define IDC_KQSoSanh                    1040
 #define IDC_SoSanh                      1041
 #define IDRESET                         1042
+#define IDC_KQDec2                      1043
 #define IDC_RESET                       1043
-
+#define IDC_KQBin2                      1044
 #define IDC_Cong                        2000
 #define IDC_Tru                         2001
 #define IDC_Nhan                        2002
 #define IDC_Chia                        2003
 #define IDC_DU                          2004
-
 #define IDC_And                         2005
 #define IDC_Or                          2006
 #define IDC_Xor                         2007
@@ -66,7 +59,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1044
+#define _APS_NEXT_CONTROL_VALUE         1046
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
